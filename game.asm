7084f4dcd963e20f96aa5679388a10be874f1f7d
Nazmus Saqeeb <50136300+namoosir@users.noreply.github.com>
2021-03-30 09:33:17 -0400

    Create game.asm

---
+++ game.asm
@@ -0,0 +1,45 @@
+#####################################################################
+#
+# CSCB58 Winter2021 Assembly Final Project
+# University of Toronto, Scarborough
+#
+# Student: Nazmus Saqeeb, 1006306007, saqeebna
+#
+# Bitmap Display Configuration:
+# - Unit width in pixels: 8 
+# - Unit height in pixels: 8 
+# - Display width in pixels: 256 
+# - Display height in pixels: 256 
+# - Base Address for Display: 0x10008000 ($gp)
+#
+# Which milestones have been reached in this submission?
+# (See the assignment handout for descriptions of the milestones)
+# - Milestone 1/2/3/4 (choose the one the applies)
+#
+# Which approved features have been implemented for milestone 4?
+# (See the assignment handout for the list of additional features)
+# 1. (fill in the feature, if any)
+# 2. (fill in the feature, if any)
+# 3. (fill in the feature, if any)
+# ... (add more if necessary)
+#
+# Link to video demonstration for final submission:
+# - (insert YouTube / MyMedia / other URL here). Make sure we can view it!
+#
+# Are you OK with us sharing the video with people outside course staff?
+# - yes / no/ yes, and please share this project githublink as well!
+#
+# Any additional information that the TA needs to know:
+# - (write here, if any)
+######################################################################
+
+.eqv	BASE_ADDRESS	0x10008000
+
+.data	
+
+.text
+
+.globl main_loop
+
+main_loop:
+	
